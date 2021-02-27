cdd18d0d5e999e3a63c9fba1d3965a75be6bf7a9
ouassimabbari <wassim.abbari@gmail.com>
2021-02-27 21:12:08 +0100

    Controls

--- webApp/index.js
+++ webApp/index.js
@@ -1,7 +1,9 @@
 sap.ui.define([
+    "sap/m/Text"
+], function (Text) {
+    "use strict";
 
-], function () {
-    "use strict"
-
-    alert("UI5 is ready to go!")
-})
+    new Text({
+        text: "Hello UI5"
+    }).placeAt("content")
+});
