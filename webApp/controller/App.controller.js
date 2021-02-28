b959bfc48caa0758ae0063b42ef87b6ad0c064e9
ouassimabbari <wassim.abbari@gmail.com>
2021-02-28 10:15:45 +0100

    JSONModel

--- webApp/controller/App.controller.js
+++ webApp/controller/App.controller.js
@@ -1,9 +1,22 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
     'use strict';
     return Controller.extend("sap.ui.demo.walkthrough.App", {
+        onInit: function () {
+
+            var oData = {
+                recipient: {
+                    name: "UI5"
+                }
+            };
+
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+
+        },
         onShowHello: function (params) {
             MessageToast.show("heeyyy");
         }
